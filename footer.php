ac824d2342f286fa0317aeae4109e9cd422e67a7
onion_apps <Devops@onion.ke>
2021-03-01 01:52:45 -0800

    commit on develop

--- footer.php
+++ footer.php
@@ -128,3 +128,20 @@
       </div>
     </div>
   </div>
+<script type="text/javascript">
+    (function () {
+        var options = {
+            facebook: "102612791869019", // Facebook page ID
+            whatsapp: "+254 797 837 351", // WhatsApp number
+            call_to_action: "Talk to Us", // Call to action
+            button_color: "#FF6550", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "facebook,whatsapp", // Order of buttons
+            pre_filled_message: "Welcome to Sama Investment your loans our priority", // WhatsApp pre-filled message
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
