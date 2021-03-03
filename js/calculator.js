da50a83fa2f1ab99e336f63fedb872cb999b4632
onion_apps <Devops@onion.ke>
2021-03-02 22:32:49 -0800

    commit on develop

--- js/calculator.js
+++ js/calculator.js
@@ -1,12 +1,10 @@
 function calculate(){
     var p = 0;
     var i = 0.04;
-    var n = 0;
+    var n = 3;
     var CI = 0;
 
     p = parseInt(document.getElementById("principal").value);
-  
-    n = parseInt(document.getElementById("number-of-period").value);
 
     CI = ((p*i*n)+p);
     document.getElementById("res").innerHTML=CI;
