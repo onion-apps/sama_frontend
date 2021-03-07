451c4bb8fa57f8c6ee698bd8913ef0996819a2cd
onion_apps <Devops@onion.ke>
2021-03-07 02:36:46 -0800

    commit on develop

--- testimonial.php
+++ testimonial.php
@@ -19,31 +19,31 @@
         <div id="testim-content" class="cont">
             <div class="container slider">
                 <div class="active">
-                    <div class="imgs"  ><img src="./images/barber.jpg" style="border-radius:50%;" alt=""></div>
+                    <div class="img"  ><img src="./images/barber.jpg" style="border-radius:50%;" alt=""></div>
                     <h2>Victor J.</h2>
                     <p>I have been receiving loans from SAMA for the last 3 years and I have no regrets. Through their support, I have been able to open my salon and barber shop. What I like most about them is their lower interest rates and flexible repayment period.
 </p>
                 </div>
 
                 <div>
-                    <div class="imgs"  ><img src="./images/grace.jpg" style="border-radius:50%;" alt=""></div>
+                    <div class="img"  ><img src="./images/grace.jpg" style="border-radius:50%;" alt=""></div>
                     <h2>Purity S.</h2>
                     <p>SAMA invested in me. Through their loans I have been able to open and sustain my business. I am able to manage and plan knowing that SAMA got me.</p>
                 </div>
 
                 <div>
-                    <div class="imgs"  ><img src="./images/sarge.jpg" style="border-radius:50%;" alt=""></div>
+                    <div class="img"  ><img src="./images/sarge.jpg" style="border-radius:50%;" alt=""></div>
                     <h2>Margret M.</h2>
                     <p>We understand that excess carbon dioxide (CO2) is building up in our atmosphere, contributing to climate change . We have been planting trees to change the narrative; one tree at a time.</p>
                 </div>
                 <div>
-                    <div class="imgs"  ><img src="./images/marg.jpg" style="border-radius:50%;" alt=""></div>
+                    <div class="img"  ><img src="./images/marg.jpg" style="border-radius:50%;" alt=""></div>
                     <h2>Vicky L.</h2>
                     <p>SAMA invested in me. Through their loans I have been able to open and sustain my business. I am able to manage and plan knowing that SAMA's got me.</p>
                 </div>
 
                 <div>
-                    <div class="imgs"  ><img src="./images/farmer.jpg" style="border-radius:50%;" alt=""></div>
+                    <div class="img"  ><img src="./images/farmer.jpg" style="border-radius:50%;" alt=""></div>
                     <h2>Mercy K.</h2>
                     <p>
 SAMA has helped me grow by being my emergency cash fund in times of need. I have risen over the levels of membership increasing my loan limits which will be of help in the future.
@@ -51,7 +51,7 @@ SAMA has helped me grow by being my emergency cash fund in times of need. I have
                 </div>
 
                 <div>
-                    <div class="imgs"  ><img src="./images/baker.jpg" style="border-radius:50%;" alt=""></div>
+                    <div class="img"  ><img src="./images/baker.jpg" style="border-radius:50%;" alt=""></div>
                     <h2>Arnest W.</h2>
                     <p>I have used SAMA over 7 months now and it has been the best experience working with them. Their fast disbursment of Loans straight to my phone during emergencies has been the main reason I choose them as my financier.</p>
                 </div>
