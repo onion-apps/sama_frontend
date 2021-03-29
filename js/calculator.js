daa1ba857411166c6a133821392d6f088b30ed32
Devops@onion.ke <Devops@onion.ke>
2021-03-29 04:04:13 -0700

    commit on develop

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,13 @@
 function calculate() {
   var interest = 0.04;
   var principal = 0;
-  if(isNaN(num1)){
-    document.write(num1 + " is not a number <br/>");
-  }
     var months = 3;
   var CI = 0;
     // Fetch values from input elements
   principal = parseInt(document.getElementById("principal").value);
+  if(isNaN(principal)){
+    document.write(principal + " is not a number <br/>");
+  }
 
   CI = principal * interest * months + principal;
   mi = CI / months;
